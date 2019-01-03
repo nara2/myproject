5d91990430d89df885b8b9eccff6658a05c1124f
Chiwon Moon <nara2084@nate.com>
2019-01-03 19:19:53 +0900

    Update beaconScan.py

--- project/raspberry/beaconScan.py
+++ project/raspberry/beaconScan.py
@@ -9,29 +9,23 @@ import time
 import MySQLdb
 import bluetooth._bluetooth as bluez
 
-db_name="gps";
-port=3306;
+db_name="";
+port=;
 #chiwon home
-chiwon_host="202.31.147.236";
-#chiwon_host ="218.151.1.61"
-chiwon_id="test";
-chiwon_pw="1234";
-#kootaek notebook
-kootaek_host="";
-kootaek_id="root1";
-kootaek_pw="root";
+host="";
+id="test";
+pw="1234";
+db="";
+port="";
 
-
-#db = MySQLdb.connect(chiwon_host, chiwon_id, chiwon_pw, "gps" , 3306);
-db = MySQLdb.connect(chiwon_host, chiwon_id, chiwon_pw, "gps" , 3306);
-#db = MySQLdb.connect("114.70.197.203", "root", "root", "gps" , 8080);
+db = MySQLdb.connect(host, id, pw, db, port);
 
 curs = db.cursor();
-#RSSI min2
-min2 = 999
+
+rssi = 999
 major = 0;
 count = 0;
-temp_min2 = 0
+temp_rssi = 0
 temp_major = 0;
 dev_id = 0
 try:
@@ -50,46 +44,34 @@ while True:
     print "----------";
     time.sleep(0.5);
     for beacon in returnedList:
-        #print beacon;
         resultList = [];
         if("24ddf4118cf1440c87cda368daf9c93e" in beacon):
             resultList.append(beacon);
             print resultList;
             for i in resultList:
-                #print len(i);
-                #major number of one
+                #major a single digit
                 if(len(i) == 62):
                     #major
                     print i[51:52];
                     #rssi
                     print i[60:62];
 
-                    #if (flag == 0):
-                    #    min2 = i[60:62];
-                    temp_min2 = int(i[60:62]);
+                    temp_rssi = int(i[60:62]);
                     temp_major = int(i[51:52]);
-                    #else :
-                    #    x = i[60:62];
-
-                #major number of two
+                #major a double digit
                 elif(len(i) == 64):
                     #major
                     print i[51:53];
                     #rssi
                     print i[62:64];
 
-                    #if(flag == 0):
-                    #    min2 = i[62:64]; 
-                    temp_min2 = int(i[62:64]);
+                    temp_rssi = int(i[62:64]);
                     temp_major = int(i[51:53]);
-                    #else :
-                    #    y = i[62:64];
-
                 else:
                     pass;
 
-                if(temp_min2 < min2):
-                    min2 = temp_min2;
+                if(temp_rssi < rssi):
+                    rssi = temp_rssi;
                     major = temp_major;
                 else:
                     pass;
@@ -97,22 +79,18 @@ while True:
         else:
             pass;
 
-    print "min2 :"
-    print min2;
-    print "temp_min2:"
-    print temp_min2;
-    
-    count+=1;
-    if(major == min2):
+	print rssi;
+	print major;
+
+    if(major == rssi):
         pass;
     else:
-        if major !=0 or min2 !=999:    
-            curs.execute("UPDATE bangle SET request_rssi = (%d) WHERE bangle_id = 2" %int(min2));
-            curs.execute("UPDATE bangle SET request_major = (%d) WHERE bangle_id = 2" %int(major));
-	if count == 10 and (major !=0 or min2 !=999):
-	    curs.execute("INSERT INTO movement(bangle_id, request_major, time) values(2, (%d), now())" %int(major));
+        if major !=0 or rssi !=999:
+            curs.execute("UPDATE bangle SET request_rssi = (%d) WHERE bangle_id = 1" %int(rssi));
+            curs.execute("UPDATE bangle SET request_major = (%d) WHERE bangle_id = 1" %int(major));
+	if major !=0 or rssi !=999:
+	    curs.execute("INSERT INTO movement(bangle_id, request_major, time) values(1, (%d), now())" %int(major));
 	    count = 0;
     db.commit()
     major = 0;
-    min2 = 999;
-
+    rssi = 999;
